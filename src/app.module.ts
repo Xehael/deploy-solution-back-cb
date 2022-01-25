f2ae88c0b52ca1fdc728c412cafc1a82c6240dd4
Xehael <cedric.boissinot99@gmail.com>
2022-01-25 13:40:52 +0100

    add change

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {MessageEntity} from "./entity/message.entity";
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
+  imports: [ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'localhost',
-    port: 3308,
-    username: 'root',
-    password: 'test',
-    database: 'test',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
     entities: [MessageEntity],
     synchronize: true,
   }),TypeOrmModule.forFeature([MessageEntity])],
